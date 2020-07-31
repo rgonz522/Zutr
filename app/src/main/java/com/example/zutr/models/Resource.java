2504a34a505988c1c403e204ee7a85aab067720d
rgonz522 <rgonz522@fiu.edu>
2020-07-30 17:07:12 -0700

    Created Resource Model and Adapter

--- app/src/main/java/com/example/zutr/models/Resource.java
+++ app/src/main/java/com/example/zutr/models/Resource.java
@@ -14,4 +14,42 @@ public class Resource {
     private String imageURL;
 
 
+    public Resource(String title, String author, String created, String imageURL) {
+        this.title = title;
+        this.author = author;
+        this.created = created;
+        this.imageURL = imageURL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 }
